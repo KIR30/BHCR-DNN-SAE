cd7df34503ad388aefed968202becfcb411daf59
Rabindra Nath Nandi <nandi@ipvision-inc.com>
2016-08-25 21:20:19 +0600

    Inculde Data and Source Code

---
+++ Bhr_DNN_sae.R
@@ -0,0 +1,24 @@
+
+#Data Loading 
+train <- read.delim("~/bangla_data/train.csv", header=FALSE)
+test <- read.delim("~/bangla_data/test.csv", header=FALSE)
+trainlabel <- read.table("~/bangla_data/trainlabel.csv", quote="\"", comment.char="")
+testlabel <- read.table("~/bangla_data/testlabel.csv", quote="\"", comment.char="")
+
+#Data Preprocessing 
+train = as.matrix(train)
+test = as.matrix(test)
+
+trainlabel = as.vector(trainlabel)
+testlabel = as.as.vector(testlabel)
+
+#Train deep neural network with weights initialized by stack autoencoder 
+
+dnn =sae.dnn.train(train, trainlabel, hidden = c(10,10), activationfun = "sigm", learningrate = 0.8, 
+              momentum = 0.5, learningrate_scale = 1, output = "sigm", sae_output = "linear", 
+              numepochs = 50, batchsize = 100, hidden_dropout = 0, visible_dropout = 0)
+
+#test
+nn.test(dnn, test, testlabel)
+
+
